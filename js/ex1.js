04e611cc74096e52cc6cc21780282524f5ea13c0
Alice <alicesb2006@gmail.com>
2024-03-28 23:29:56 -0300

    Criando o programa para o exercício 1 - Nicolas Haubricht Hainfellner GitHub: https://github.com/NicolasHaubricht

---
+++ js/ex1.js
@@ -0,0 +1,15 @@
+//Exercício 1
+console.log("Exercicio 1")
+//Variável indefinida
+let indefinido;
+//Variável vazia
+let vazio = null;
+//Variável com valor
+let valor = 27;
+
+
+//Printa os valores da variável no console
+console.log(indefinido);
+console.log(vazio);
+console.log(valor);
+
