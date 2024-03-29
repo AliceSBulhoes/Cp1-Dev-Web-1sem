73d2b6aeef45fb6390333e4858c346afb6acbc97
Alice <alicesb2006@gmail.com>
2024-03-28 23:40:10 -0300

    Criando o programa do exercício 9 - Lucas Henzo Ide Yuki  Github :https://github.com/LucasYuki1

---
+++ js/ex9.js
@@ -0,0 +1,9 @@
+console.log("Exercício 9")
+//Declarando a o numero String
+let numstr = "123.123";
+
+//Convertendo a str em float
+let numfloat = parseFloat(numstr);
+
+//Mostrando no console o tipo da variável
+console.log(typeof(numfloat));
