91b85a36ffa6b54fc934cc869e85461197ed343c
Alice <alicesb2006@gmail.com>
2024-03-28 23:31:57 -0300

    Criando o programa do exercício 2 - Eduardo Oliveira Cardoso Madid Github :https://github.com/EduardoMadid

---
+++ js/ex2.js
@@ -0,0 +1,15 @@
+//Ex2
+console.log("Exercicio 2")
+//Utilizando >= 
+console.log(5 >= 3); // Mostra no console true, porque 5 é maior que 3.
+console.log(5 >= 5); // Mostra no console true, porque 5 não é maior, mais é igual a 5. 
+console.log(5 >= 7); // Mostra no console false, porque 5 é menor que 7.
+
+//Utilizando ==
+console.log(5 == 3); // Mostra no console false, porque 5 não é equivalente a 3.
+console.log(5 == 5); // Mostra no console true, porque 5 é equivalente a 5.
+
+//Utilizando !=
+console.log(5 != 3); // Mostra no console true, porque 5 é diferente de 3.
+console.log(5 != 5); // Mostra no console false, porque 5 é igual a 5.
+
